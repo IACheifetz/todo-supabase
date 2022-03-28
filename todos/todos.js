2248410d42e915ccc35d7d42368635801d914e80
Ian Cheifetz <sylvan.iacheifetz09@gmail.com>
2022-03-28 12:27:13 -0700

    added exported functions to create delete and get to do lists in fetch utils and a render function for to do lists

--- todos/todos.js
+++ todos/todos.js
@@ -16,17 +16,34 @@ const logoutButton = document.querySelector('#logout');
 const deleteButton = document.querySelector('.delete-button');
 
 todoForm.addEventListener('submit', async(e) => {
-    // on submit, create a todo, reset the form, and display the todos
+    e.preventDefault();
+
+    const formData = new FormData(todoForm);
+
+    const todo = formData.get('todo');
+
+    await createTodo(todo);
+
+    todoForm.reset();
+    displayTodos();
 });
 
 async function displayTodos() {
-    // fetch the todos
+    const todos = await getTodos();
     
-    // display the list of todos
+    todosEl.textContent = '';
 
-    // be sure to give each todo an event listener
+    for (let todo of todos) {
+        const todoEl = renderTodo(todo);
 
-    // on click, complete that todo
+        todoEl.addEventListener('click', async() => {
+            await completeTodo(todo.id);
+
+            displayTodos();
+        });
+
+        todosEl.append(todoEl);
+    }
 }
 
 // add an on load listener that fetches and displays todos on load
